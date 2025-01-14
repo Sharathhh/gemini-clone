9fb4b231e89a79d58c5d3ee2a56e84e357a69435
Sharath <sharathu0123@gmail.com>
2025-01-15 00:30:20 +0530

    fixed

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../Context/Context'
+
+
+
+
 function Main() {
+    const { prevPrompts, setPrevPrompts, onSent, setRecentPrompt, recentPrompt, showResult, loading, setResultData, resultData, input, setInput, } = useContext(Context)
     return (
         <div className='main'>
             <div className="nav">
@@ -9,42 +15,62 @@ function Main() {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello,Dev</span></p>
-                    <p>How can I help you today?</p>
-                </div>
-                <div className="cards">
-                    <div className='card'>
-                        <p>Suggest beautifull places</p>
-                        <img src={assets.compass_icon} alt="" />
-                    </div>
+                {!showResult ?
+                    <>
+                        <div className="greet">
+                            <p><span>Hello,Dev</span></p>
+                            <p >How can I help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className='card'>
+                                <p >Suggest beautifull places</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
 
-                    <div className='card'>
-                        <p>Suggest beautifull places </p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className='card'>
-                        <p>Suggest beautifull places</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className='card'>
-                        <p>Suggest beautifull places</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
+                            <div className='card'>
+                                <p>Suggest beautifull places </p>
+                                <img  src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className='card'>
+                                <p>Suggest beautifull places</p>
+                                <img src={assets.message_icon} alt="" />
+                            </div>
+                            <div className='card'>
+                                <p>Suggest beautifull places</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                        </div>
+
+                    </> : <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading?<div className='loader'>
+                                <hr />
+                                <hr />
+                                <hr />
+                            </div>:
+                            <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                            }
+                        </div>
+                        </div>}
+
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder='Enter a prompt here' />
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
 
                         <div>
-                            <img src={assets.gallery_icon}  />
+                            <img src={assets.gallery_icon} />
                             <img src={assets.mic_icon} />
-                            <img src={assets.send_icon}  />
+                           {input? <img onClick={() => onSent()} src={assets.send_icon} />:null}
                         </div>
                     </div>
                     <p className="bottom-info">
-                            Gemini may display inaccurate info, includes about people, so double-check its answers
-                        </p>
+                        Gemini may display inaccurate info, includes about people, so double-check its answers
+                    </p>
                 </div>
             </div>
 
--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,34 +1,36 @@
 import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai";
-  
-  const apiKey = "AIzaSyB_Ik-yHS_76UIRgx4Wqw-eaNONPB0qmfA";
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-exp",
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// Accessing environment variable from Vite
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chat = model.startChat({
+    generationConfig,
+    history: [],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function runChat(prompt) {
-    const chat = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chat.sendMessage(prompt);
-    const response=result.response;
-    console.log(result.response.text());
-  }
-  
-  export default runChat;
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(result.response.text());
+  return response.text();
+}
+
+export default runChat;
